0feca7d49211afaa4ff3ba1792dad9174cc9dec4
Thomas <tc.messerer@gmail.com>
2026-05-01 17:16:33 +0000

    6-9: pycodestyle corrections

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 digit_collection = ""
+
+
 def print_last_digit(number):
     global digit_collection
     digit_collection += str(number % 10)
     print(digit_collection)
-
-print_last_digit(9)
