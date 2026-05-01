a862688fe277da9f4f15aedbda4bad48e0950ff2
Thomas <tc.messerer@gmail.com>
2026-05-01 14:01:10 +0000

    6-8: added file 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def defuppercase(str):
+    all_uppercase = ""
+    for a in str:
+        if ord(a) in range(97, 123):
+            all_uppercase += chr((ord(a) - 32))
+        else:
+            all_uppercase += chr(ord(a))
+    print(all_uppercase)
