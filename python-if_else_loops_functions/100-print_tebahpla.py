7d68c7b2a4cb2af5a99cce98ac558ae3b0aec795
Thomas <tc.messerer@gmail.com>
2026-05-02 19:03:34 +0000

    adjusted script

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
-    if i % 2 == 0 and i > 96:
-        print(chr(i), end="")
-    elif i % 2 == 1 and i > 95:
-        print(chr(i-32), end="")
-    else:
-        pass
+for i in range(122, 96, -1):
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
