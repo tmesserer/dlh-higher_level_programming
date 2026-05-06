dc5abef36cf40b34645b2bcabe965064e8b0c159
Thomas Messerer <tc.messerer@gmail.com>
2026-05-06 19:14:14 +0000

    1: adjusted file 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if 0 <= idx <= len(my_list)-1:
-        print("Element at index {:d} is {}".format(idx, my_list[idx]))
+        return ("Element at index {:d} is {}".format(idx, my_list[idx]))
     else:
-        print("None")
+        return ("None")
