56bbb55ec64b7da4d7f8e2e333a5a57a4ca8bb57
Thomas Messerer <tc.messerer@gmail.com>
2026-05-06 18:56:19 +0000

    0: added format string

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for digit in range(0, len(my_list)):
-        print(my_list[digit])
+        print("{}".format(my_list[digit]))
+print_list_integer([1, 2, 3, 4, 5])
