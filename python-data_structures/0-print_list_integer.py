cad4cd5934decf090d20ea6c24bf5a932e450f6a
Thomas Messerer <tc.messerer@gmail.com>
2026-05-06 19:05:26 +0000

    0: adjusted file 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for digit in range(0, len(my_list)):
-        print("{}".format(my_list[digit]))
-print_list_integer([1, 2, 3, 4, 5])
+    for digit in my_list:
+        print("{:d}".format(digit))
