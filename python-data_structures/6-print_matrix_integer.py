539268c282dbe4edb1f5ee90903823288b9b6655
Thomas <tc.messerer@gmail.com>
2026-05-07 18:48:25 +0000

    6: used print instead of return

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if isinstance(matrix, list):
-        integer_matrix = ""
         for part in matrix:
             for i, number in enumerate(part):
                 if i != len(part)-1:
-                    integer_matrix += "{:d} ".format(number)
+                    print("{:d}".format(number), end=" ")
                 else:
-                    integer_matrix += "{:d}\n".format(number)
-        return (integer_matrix)
+                    print("{:d}\n".format(number), end="")
     else:
-        return (matrix)
-# print(print_matrix_integer([[1,2,3], [4,5,6], [7,8,9]]))
+        print(matrix)
