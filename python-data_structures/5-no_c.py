e78cbef0a52955767cd85fd50579a7a23ae804df
Thomas <tc.messerer@gmail.com>
2026-05-07 18:06:09 +0000

    5: new_string variable that gets added with for loop

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     for letter in my_string:
         if letter == "c":
             pass
         elif letter == "C":
             pass
         else:
-            print("{}".format(letter), end="")
+            new_string += letter
+    return (new_string)
