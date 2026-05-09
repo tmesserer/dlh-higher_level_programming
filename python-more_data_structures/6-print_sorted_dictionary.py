6e9c307e2bc753317c674bd567aee27d32e4e456
Thomas Messerer <tc.messerer@gmail.com>
2026-05-09 17:34:10 +0000

    6: adjusted with .items for dicts

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return sorted(a_dictionary.keys())
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
