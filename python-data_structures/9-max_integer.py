3435460e6c59a4cdc90d6aef4e43320ad6e498ce
Thomas Messerer <tc.messerer@gmail.com>
2026-05-09 11:06:16 +0000

    9: adjusted logic for negative values

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,10 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    max_value = 0
+    max_value = ""
     for i in my_list:
-        if i > max_value:
+        if max_value == "":
+            max_value = i
+        elif i > max_value:
             max_value = i
     return (max_value)
