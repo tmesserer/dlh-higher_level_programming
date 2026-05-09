1eb46ee28844b4b019a9cf3c6f3e24a0859f737a
Thomas Messerer <tc.messerer@gmail.com>
2026-05-09 11:48:47 +0000

    11: much simpler, remove value; problem with multiple same integers

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,11 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list)-1:
         return my_list
     else:
-        new_list = []
-        for i in range(0, len(my_list)):
-            if i == idx:
-                pass
-            else:
-                new_list.append(my_list[i])
-        my_list = new_list
-        return new_list
+        my_list.remove(my_list[idx])
+        return my_list
