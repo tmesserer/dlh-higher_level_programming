7e713babede44c199a2b2656ecb115150732bed9
Thomas Messerer <tc.messerer@gmail.com>
2026-05-09 18:12:25 +0000

    7: used return instead of print at end of function

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    for key, value in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, value))
+    return a_dictionary
