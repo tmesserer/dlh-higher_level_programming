d8c560ed3c9f6ffb19998bdfd369f93568afd44c
Thomas Messerer <tc.messerer@gmail.com>
2026-05-09 19:03:11 +0000

    8: added file 8

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is None:
+        return a_dictionary
+    else:
+        del a_dictionary[key]
+    return a_dictionary
