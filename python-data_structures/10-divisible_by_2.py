a9459ade3905b4f63a54db4f9d836b9e08441184
Thomas Messerer <tc.messerer@gmail.com>
2026-05-09 11:21:34 +0000

    10: added length of range

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     divisible_list = []
-    for i in range(0, len(my_list)-1):
+    for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
             divisible_list.append(True)
         else:
