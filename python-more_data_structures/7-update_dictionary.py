97a063d99101aa7510779ffbe1c93902f6a77a9f
Thomas Messerer <tc.messerer@gmail.com>
2026-05-09 18:02:40 +0000

    7: adjusted logic for vertical unpacking

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    print(a_dictionary)
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
