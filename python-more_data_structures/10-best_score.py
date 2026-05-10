beac7a599d693160c59bcacb76c30e3e5bc10de9
Thomas Messerer <tc.messerer@gmail.com>
2026-05-10 14:54:00 +0000

    10: adjusted logic for empty dict given

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None or not isinstance(a_dictionary, dict):
         return None
+    elif a_dictionary == {}:
+        return None
     else:
         best_score_key = ""
         best_score_value = 0
