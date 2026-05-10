7f4ab24777e8bc08989b6a2ffdb3838a242bf931
Thomas Messerer <tc.messerer@gmail.com>
2026-05-10 17:43:33 +0000

    0: added return of nb_print as log of loops iterated

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    nb_print = 0
     for i in range(0, x):
         try:
             print(my_list[i], end="")
-        except (IndexError, TypeError):
+            nb_print += 1
+        except IndexError:
             break
     print()
+    return nb_print
