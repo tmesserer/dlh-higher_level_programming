237ab2f5c2ca44958d2cb66c61de31c2c172cbe2
Thomas Messerer <tc.messerer@gmail.com>
2026-05-10 18:38:08 +0000

    12: added file

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+
+# I = 1
+# V = 5
+# X = 10
+# L = 50
+# C = 100
+# D = 500
+# M = 1000
+# --> dictionary?
