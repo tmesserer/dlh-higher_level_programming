e88ec5b4934557b47a6f634fbc4606c589435b20
Thomas Messerer <tc.messerer@gmail.com>
2026-05-14 18:36:54 +0000

    7: restored file 100 to previous version

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
-
-
-def safe_function(fct, *args):
+def safe_print_integer_err(value):
+    from sys import stderr
     try:
-        return fct(args*)
+        print("{:d}".format(value), end="\n")
+        return True
     except Exception as error:
-        print("Exception: {}".format(error), file=sys.stderr)
-        return None
+        print(f"Exception: {error}", file=stderr)
+        return False
