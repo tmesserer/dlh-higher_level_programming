52a735c49a4fd239394d8b5ce85c5d418989495c
Thomas Messerer <tc.messerer@gmail.com>
2026-05-14 18:33:48 +0000

    8: formating changes

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import stderr
+import sys
+
+
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
-        return result
+        return fct(args*)
     except Exception as error:
-        print("Exception: {}".format(error), file=stderr)
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
