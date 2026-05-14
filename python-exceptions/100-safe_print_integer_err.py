e91e380b54a93dc8032dd3740d1008615c122c6a
Thomas Messerer <tc.messerer@gmail.com>
2026-05-14 18:26:18 +0000

    8: adjusted import to top, changed f-string to format()

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_function(fct, *args):
-    from sys import stderr
+from sys import stderr
+def safe_function(fct, *args):    
     try:
         result = fct(*args)
         return result
     except Exception as error:
-        print(f"Exception: {error}", file=stderr)
+        print("Exception: {}".format(error), file=stderr)
         return None
