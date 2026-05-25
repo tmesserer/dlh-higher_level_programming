e4df602f3952b88455abee3932b876824c24b878
Thomas <tc.messerer@gmail.com>
2026-05-25 13:35:02 +0200

    0: added docstring for module

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Module about rectangle class and its methods and attributes"""
+
+
 class Rectangle:
     """
-    This creates the rectangle class. 
+    This creates the rectangle class.
     A closed figure of 4 sides, the opposite ones are of equal length.
     Aditionally, all angles are 90°.
     """
+
     pass
