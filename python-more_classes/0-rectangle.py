2bc70339de699a871404eb5170f03112b6f89678
Thomas <tc.messerer@gmail.com>
2026-05-25 13:29:09 +0200

    0: added file 0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    This creates the rectangle class. 
+    A closed figure of 4 sides, the opposite ones are of equal length.
+    Aditionally, all angles are 90°.
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is a readme for all the scripts in this folder
