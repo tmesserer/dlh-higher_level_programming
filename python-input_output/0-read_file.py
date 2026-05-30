eede5e743b5292ee44650352a8fc851055992dd1
Thomas <tc.messerer@gmail.com>
2026-05-30 14:43:15 +0200

    1: added files 1-write_file and my_file_1.txt

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This module is used for the project input/output.
-Currently there is a function that reads a file.
+I/O utilities for reading and displaying project data files.
+Focuses currently on UTF8 text files.
 """
 
 
 def read_file(filename=""):
-    """This function opens a text file (UTF8),
-    then prints its content one line after another"""
+    """Open a text file (UTF8),
+    then printsthe contents to stdout."""
     with open(filename) as f:
         for line in f:
             print(line, end="")
