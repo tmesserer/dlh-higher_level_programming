e9f34a8b3863ba69aa02c469e4b79e5a0d12630e
Thomas <tc.messerer@gmail.com>
2026-05-30 13:51:02 +0200

    0: added docstring for module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module is used for the project input/output.
+Currently there is a function that reads a file.
+"""
+
+
 def read_file(filename=""):
     """This function opens a text file (UTF8),
     then prints its content one line after another"""
