18fde2d108919dfaa4ee664239504c60778f33a5
Thomas <tc.messerer@gmail.com>
2026-05-30 15:11:58 +0200

    2: added files

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module is used for the project input/output.
+Currently there is a function that reads a file.
+"""
+
+
+def write_file(filename="", text=""):
+    """Write text to a UTF-8 file, creating it if it doesn't exist.
+    Args:
+        filename: path to file to write
+        text: text to write in file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
