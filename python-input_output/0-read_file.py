0b0cc3645499e4ef9c47b36cbf7f97fcc24898d7
Thomas <tc.messerer@gmail.com>
2026-05-30 13:38:35 +0200

    0: added file 0-read_file and my_file_0.txt

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+def read_file(filename=""):
+    """This function opens a text file (UTF8),
+    then prints its content one line after another"""
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
