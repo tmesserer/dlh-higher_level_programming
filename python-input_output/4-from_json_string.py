1882b402dbc5bc5c94e1b475592422d0954428e5
Thomas <tc.messerer@gmail.com>
2026-05-31 15:34:24 +0200

    4: added files

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -5,12 +5,12 @@ It defines a function that creates a json
 """
 
 
-def to_json_string(my_obj):
+def from_json_string(my_str):
     """
-    Transfrom text to json string.
+    Transfrom json string to python object.
 
     Args:
         my_obj: test to convert
     """
     import json
-    return json.dumps(my_obj)
+    return json.loads(my_str)
