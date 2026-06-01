461100d809e410a9ab9c3225812d01268c81f782
Thomas <tc.messerer@gmail.com>
2026-06-01 11:56:35 +0200

    1: added file

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Modul used for serialization and de-serialization"""
+import pickle
+
+
+class CustomObject:
+    """
+    This creates the CustomObject class.
+    """
+
+    def __init__(self, name, age, is_student):
+        """
+        Args:
+            name:
+            age:
+            is_student:
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """displays all attributes in dictionary format"""
+        for key, value in self.__dict__.items():
+            print("{}: {}".format(key, value))
+
+    def serialize(self, filename):
+        """serializes its attributes in the attribute filename"""
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """This class method will take a filename as its parameter.
+        Using the pickle module, it will load and return an instance of
+        the CustomObject from the provided filename.
+        """
+        with open(filename, "rb") as f:
+            return pickle.load(f)
