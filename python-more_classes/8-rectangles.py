33351cf93004852319af2cf509196e7a02c8f44b
Thomas <tc.messerer@gmail.com>
2026-06-01 09:30:00 +0200

    8: adjusted files

--- python-more_classes/8-rectangles.py
+++ python-more_classes/8-rectangles.py
@@ -108,3 +108,15 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
         del (self)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """This method returns the bigger of two rectangles"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        else:
+            return rect_1
